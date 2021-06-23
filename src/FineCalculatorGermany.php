e862c311aa3d8779ef167a27410ff6f3111504e6
Daniel Hollfelder <hollfelder_daniel@yahoo.de>
2021-06-23 23:27:31 +0200

    neuer versuch mit pushen

--- src/FineCalculatorGermany.php
+++ src/FineCalculatorGermany.php
@@ -2,10 +2,54 @@
 namespace SpeedControlKata;
 
 class FineCalculatorGermany {
+    /**
+     * @param int $allowedSpeed
+     * @param int $actualSpeed
+     * @param bool $isInCity
+     * @return Fine
+     *
+     */
 
     public function getFine(int $allowedSpeed, int $actualSpeed, bool $isInCity)
     {
-        return new Fine(0,0,0);
-    }
+        $ToleranzSpeed = ($actualSpeed/100) * 97;
+        $diffSpeed = ($ToleranzSpeed <= $allowedSpeed) ? false : $ToleranzSpeed - $allowedSpeed;
 
+        $jsontempo = file_get_contents(__DIR__ . '/../json/tempostrafen.json');
+        $jsonarray = json_decode($jsontempo, true);
+
+        /**
+         * der Wert bei $key['totempo'] ist maxtempo in strafbereich, 00 ist unendlich
+         * $diffspeed wird überprüft ob man zu schnell war
+         * Ist diffspeed zu schnell wir aus denn Json array die aktuelle Strafe ermittel
+         */
+
+        if ($diffSpeed <> false){
+           foreach ($jsonarray as $key){
+              if($diffSpeed >= $key['oftempo'] AND ($diffSpeed <= $key['totempo'] OR $key['totempo'] == 00 )){
+                 if ($isInCity == true ){
+                    $feeamount = $key['insideplace_price'];
+                    $points = $key['insideplacepoint'];
+                    $withdrawMonth = $key['insideplace_drivingban_monat'];
+                    $feedback = 'Diesmal kostet ihnen das Foto '.$feeamount.'Euro';
+                 }else{
+                    $feeamount = $key['outsideplace_price'];
+                    $points = $key['outsideplacepoint'];
+                    $withdrawMonth = $key['outsideplace_drivingban_monat'];
+                    $feedback = 'Diesmal kostet ihnen das Foto '.$feeamount.' Euro';
+                 }
+
+                 $feedback .= ($withdrawMonth <> 0) ? ' und Sie müssen '. $withdrawMonth.' Monat laufen.' : '.';
+              }
+           }
+        }else{
+
+            $feeamount = 0;
+            $points = 0;
+            $withdrawMonth = 0;
+            $feedback = 'Tut mir Leid... sie waren leider so Langsam, vielleicht das nexte mal';
+        }
+
+        return new Fine($feeamount, $points, $withdrawMonth, $feedback);
+    }
 }
--- tests/FineCalculatorGermanyTest.php
+++ tests/FineCalculatorGermanyTest.php
@@ -6,11 +6,33 @@ use SpeedControlKata\FineCalculatorGermany;
 use SpeedControlKata\Fine;
 
 class FineCalculatorGermanyTest extends TestCase {
+
     public function testZeroKmh() {
-        $fine = new Fine(0,0,0);
-        $fineCalculator = new FineCalculatorGermany(); 
-        $fine = $fineCalculator->getFine(0,0,false);
+        $fine = new Fine(0,0,0, '');
+        $fineCalculator = new FineCalculatorGermany();
+        $fine = $fineCalculator->getFine(0,0,false, '');
         $this->assertEquals(0, $fine->feeAmount);
     }
+    /**
+     * $feedback ist das der richtige weg ?c
+     *
+     */
+    public function testmaxKmh() {
+        $feedback = 'ist ein test';
+        $fine = new Fine(680,3,3, $feedback);
+        $fineCalculator = new FineCalculatorGermany();
+        $fine = $fineCalculator->getFine(80,200,true);
+        $this->assertEquals(680, $fine->feeAmount);
+    }
 
+    /**
+     * Das man bei null nix bezahlen muss
+     */
+    public function testminKmh() {
+        $feedback = 'ist ein test';
+        $fine = new Fine(0,0,0, $feedback);
+        $fineCalculator = new FineCalculatorGermany();
+        $fine = $fineCalculator->getFine(80,80,true);
+        $this->assertEquals(0, $fine->feeAmount);
+    }
 }
